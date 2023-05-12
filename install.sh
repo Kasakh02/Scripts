a62577bfb82c91fbcbcb33d9a2b74df1804f5e89
Henrique Correa Villa Costa De Moraes <hcorrea-@c2r10s7.42lisboa.com>
2023-05-12 16:34:22 +0100

    Script fixed

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ git clone https://github.com/Kasakh02/Scripts.git || exit
 mkdir .scripts || exit
 cp Scripts/*.py ./.scripts
 
-ZSH_FILE="./.zshrc"
-BASH_FILE="./.bashrc"
+ZSH_FILE="$HOME/.zshrc"
+BASH_FILE="$HOME/.bashrc"
 
 if !grep "new=" "$ZSH_FILE" &> !grep "new=" "$BASH_FILE" &> /dev/null; then
 	printf "alias new=%s/python3 $HOME/.scripts/new_project.py\n" "$HOME" >> "$ZSH_FILE"
