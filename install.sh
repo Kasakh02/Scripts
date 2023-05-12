d28861e3cc6ee33cf023e48f6e53eafde13463bd
Henrique Correa Villa Costa De Moraes <hcorrea-@c2r10s7.42lisboa.com>
2023-05-12 16:58:35 +0100

    Script fixed

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd "$HOME" || exit
 
 git clone https://github.com/Kasakh02/Scripts.git || exit
@@ -9,12 +11,12 @@ ZSH_FILE="$HOME/.zshrc"
 BASH_FILE="$HOME/.bashrc"
 
 if ! grep "new=" "$ZSH_FILE" &> ! grep "new=" "$BASH_FILE" &> /dev/null; then
-	printf "alias new=\"python3 %s/.scripts/new_project.py\"\n" "$HOME" >> "$ZSH_FILE"
+	printf "\nalias new=\"python3 %s/.scripts/new_project.py\"\n" "$HOME" >> "$ZSH_FILE"
 	printf "\nalias new=\"python3 %s/.scripts/new_project.py\"\n" "$HOME" >> "$BASH_FILE"
 	echo "Alias \"new\" successfully added to .zshrc and .bashrc\n"
 fi
 
 rm -rf "$HOME/Scripts"
-#cd -
+cd -
 
 exec "$SHELL" || exit
