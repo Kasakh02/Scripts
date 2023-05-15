ea686d61c72cf01e9ccc0f593da46004ac5bb9e3
Henrique Moraes <kikovillamoraes@gmail.com>
2023-05-15 16:03:14 +0100

    Some small fixes

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ cp Scripts/*.py ./.scripts
 ZSH_FILE="$HOME/.zshrc"
 BASH_FILE="$HOME/.bashrc"
 
-if ! grep "new=" "$ZSH_FILE" &> ! grep "new=" "$BASH_FILE" &> /dev/null; then
+if ! (grep "new=" "$ZSH_FILE" &> /dev/null) && ! (grep "new=" "$BASH_FILE" &> /dev/null); then
 	printf "\nalias new=\"python3 %s/.scripts/new_project.py\"\n" "$HOME" >> "$ZSH_FILE"
 	printf "\nalias new=\"python3 %s/.scripts/new_project.py\"\n" "$HOME" >> "$BASH_FILE"
 	echo "Alias \"new\" successfully added to .zshrc and .bashrc\n"
