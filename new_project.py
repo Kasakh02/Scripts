ab0df27cc39f9d21d4cca4ff8dca7a7a2d6e813b
Henrique Moraes <kikovillamoraes@gmail.com>
2023-05-15 19:53:08 +0100

    Class and New 100%  done

--- new_project.py
+++ new_project.py
@@ -24,3 +24,5 @@ for i in range(nbr_exs):
 	os.mkdir(main_folder + "/ex0" + (str)(i))
 	os.mkdir(main_folder + "/ex0" + (str)(i) + "/src")
 	os.mkdir(main_folder + "/ex0" + (str)(i) + "/inc")
+
+print(Fore.GREEN + 'Success: ' + Fore.RESET + ' all exercises from project ' + main_folder + 'created')
