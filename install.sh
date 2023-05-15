33968a3fd0c1a2b3d2e87363526f73cba4e92ef5
Henrique Moraes <kikovillamoraes@gmail.com>
2023-05-15 15:18:13 +0100

    Some small fixes

--- install.sh
+++ install.sh
@@ -4,13 +4,16 @@ cd "$HOME" || exit
 
 git clone https://github.com/Kasakh02/Scripts.git || exit
 
-mkdir .scripts || echo "File .scripts already exists"
+if !mkdir .scripts; then
+echo "File .scripts already exists"
+fi
+
 cp Scripts/*.py ./.scripts
 
 ZSH_FILE="$HOME/.zshrc"
 BASH_FILE="$HOME/.bashrc"
 
-if ! grep "new=" "$ZSH_FILE" &> ! grep "new=" "$BASH_FILE" &> /dev/null; then
+if !grep "new=" "$ZSH_FILE" &> !grep "new=" "$BASH_FILE" &> /dev/null; then
 	printf "\nalias new=\"python3 %s/.scripts/new_project.py\"\n" "$HOME" >> "$ZSH_FILE"
 	printf "\nalias new=\"python3 %s/.scripts/new_project.py\"\n" "$HOME" >> "$BASH_FILE"
 	echo "Alias \"new\" successfully added to .zshrc and .bashrc\n"
